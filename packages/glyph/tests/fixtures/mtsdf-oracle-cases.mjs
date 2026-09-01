1ad2ef991529e73e08a0500b15467ace87a3d592
Justin Walsh <contact.me@thejustinwalsh.com>
2026-08-31 20:48:41 -0400

    fix(mtsdf): protect corners and edges from error correction (#135)

--- packages/glyph/tests/fixtures/mtsdf-oracle-cases.mjs
+++ packages/glyph/tests/fixtures/mtsdf-oracle-cases.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71864313f63b0a1e2663fd9d332d7841c7f9d8f499aba15f42ad2176ac141a05
+oid sha256:18df589b8bf5b32ce67a69d23399c36237183a5c66dfbfa00bf272476ba674f0
 size 2883
